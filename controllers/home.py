f49f93fb9c62ddeb09fbf05d3bbcabd06e2141f1
Damien Detcherry <damien@phonoblaster.com>
2013-11-02 16:51:02 +0100

    author associated to idea in homepage

--- controllers/home.py
+++ controllers/home.py
@@ -1,3 +1,5 @@
+from google.appengine.ext import db
+
 from controllers.base import BaseHandler
 
 from models.idea import Idea
@@ -5,10 +7,13 @@ from models.idea import Idea
 class HomeHandler(BaseHandler):
 	def get(self):
 		ideas = Idea.all().order("-created").fetch(50)
-		# TODO: pagination for ideaes
+		author_keys = []
+		for idea in ideas:
+			author_keys.append(Idea.author.get_value_for_datastore(idea))
+		authors = db.get(author_keys)
 		
 		values = {
-			"ideas" : ideas,
+			"feed": zip(ideas, authors),
 		}
 		path = "home.html"
 		self.render(path, values)
