d98543283c0c1f56e607792f720c6e274d036176
Damien Detcherry <damien@phonoblaster.com>
2013-12-30 20:37:59 +0100

    welcome page removed, homepage shows links

--- controllers/home.py
+++ controllers/home.py
@@ -7,19 +7,16 @@ from models.idea import Idea
 class HomeHandler(BaseHandler):
 	def get(self):
 		values = {}
-		if(self.current_user):
-			#ideas = Idea.all().filter("country =", self.current_user.country).order("-created").fetch(50)
-			ideas = Idea.all().order("-created").fetch(50)
-			author_keys = []
-			for idea in ideas:
-				author_keys.append(Idea.author.get_value_for_datastore(idea))
-			authors = db.get(author_keys)
-		
-			values.update({
-				"feed": zip(ideas, authors),
-			})		
-			path = "home.html"
-		else:
-			path = "welcome.html"
+		#ideas = Idea.all().filter("country =", self.current_user.country).order("-created").fetch(50)
+		ideas = Idea.all().order("-created").fetch(50)
+		author_keys = []
+		for idea in ideas:
+			author_keys.append(Idea.author.get_value_for_datastore(idea))
+		authors = db.get(author_keys)
+	
+		values.update({
+			"feed": zip(ideas, authors),
+		})		
+		path = "home.html"
 	
 		self.render(path, values)
